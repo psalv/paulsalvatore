f04ecc70b45b16fd561244715917f3b99f20e5f3
psalv <psalvato@uwo.ca>
2017-05-28 16:24:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Paul Salvatore
+Personal Website
---
+++ public_html/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <!--=========================================== WEBPAGE METADATA ====================================-->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Blank Heroku Project</title>
+    <!-- Favicons
+    ================================================== -->
+    <link rel="manifest" href="assets/img/favicon/manifest.json">
+
+
+    <!--=========================================== CSS FILES ===========================================-->
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+    <!-- Custom Fonts -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+
+</head>
+
+<body>
+
+<!--=========================================== MAIN FILES ==========================================-->
+<?php
+$page = "index-page";
+$hasGmap = true;
+?>
+
+
+<!--=========================================== JS SCRIPTS ==========================================-->
+<!-- jQuery -->
+<script src="assets/js/jquery.min.js" type="text/javascript"></script>
+
+
+<!-- Bootstrap Core JavaScript -->
+<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+
+<!-- Plugin JavaScript -->
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+<!-- Theme JavaScript -->
+
+
+<!-- Additional JS Files-->
+
+
+
+</body>
+</html>
