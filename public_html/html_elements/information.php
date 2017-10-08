cb711b9241322e10b37f79cea20d69d7498fd97a
psalv <psalvato@uwo.ca>
2017-10-08 17:16:10 -0400

    Changed TC#1 to be more material design.

---
+++ public_html/html_elements/information.php
@@ -0,0 +1,17 @@
+
+<div class="row informationContainer">
+
+    <!-- This is the container for picking the information -->
+    <div class="col-xs-6 containerLeft">
+        <div class="centerContainer">
+            <div class="centerElement">TEST</div>
+        </div>
+    </div>
+
+    <!-- In this container the information will be displayed -->
+    <div class="col-xs-6 containerRight">
+
+    </div>
+
+
+</div>
--- public_html/html_elements/traincar.php
+++ public_html/html_elements/traincar.php
@@ -5,23 +5,26 @@
 
 <div class="traincar-car" data-car-id='1' id="car-1" style="top: -100%">
 <!--second car-->
-    <div class="content-block">
-        <div class="cycleWords">
-            I enjoy <span id="enjoyRotation">problem solving.</span>
-        </div>
-        <div class="text-center hidden" id="wii">
-            (I am my family's reigning champion)
-        </div>
-        <div class="cycleWords">
-            I have been <span id="beenRotation">a developer.</span>
+    <div class="card">
+        <div class="content-block">
+            <div class="cycleWords">
+                I enjoy <span id="enjoyRotation">problem solving.</span>
+            </div>
+            <div class="hidden" id="wii">
+                (I am my family's reigning champion)
+            </div>
+            <div class="cycleWords">
+                I have been <span id="beenRotation">a developer.</span>
+            </div>
         </div>
     </div>
+
+    <div class="backgroundLeft"></div>
 
 </div>
 
 <div class="traincar-car" data-car-id='2' id="car-2" style="top: -100%">
-<!--third car-->
-    THIRD
+    <?php include "information.php"; ?>
 </div>
 
 <div class="traincar-car" data-car-id='3' id="car-3" style="top: -100%">
